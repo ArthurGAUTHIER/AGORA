e926815153bdebab10d398601d35727977e75957
Arthur GAUTHIER <40882617+ArthurGAUTHIER@users.noreply.github.com>
2018-08-28 18:41:34 +0200

    Update actor.rb

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,5 @@
 class Actor < ApplicationRecord
-  validates :last_name, presence: true
+  validates :last_name, presence: true, uniqueness: true
   validates :first_name, presence: true
   has_many :media_actors
 end
