c7435b610e42096958846623bb2c91f13cc33089
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-08-28 19:25:12 +0200

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath    = 'db/IMDB-Movie-Data.csv'
+
+movies = [];
+
+CSV.foreach(filepath, csv_options) do |row|
+  row['Genre'].split(',').each do |genre|
+    categorie = Category.find_by(name: genre)
+    if categorie.nil?
+      Category.create(
+        name: genre
+        )
+    end
+  end
+
+  row['Actors'].split(',').each do |actor|
+    names = actor.split(',')
+
+    if names.length > 2
+      names = [names[0..(names.length - 1), names.last]]
+    end
+
+    first_name = names[0]
+    last_name = names[1]
+
+    actor = Actor.find_by(first_name: first_name)
+    if actor.nil?
+      Actor.create(
+        first_name: first_name
+        last_name: last_name
+        )
+    end
+  end
+
+  names = row['Director'].split()
+
+    if names.length > 2
+      names = [names[0..(names.length - 1), names.last]]
+    end
+
+    first_name = names[0]
+    last_name = names[1]
+
+  Director.create(
+    first_name: first_name
+    last_name: last_name
+    )
+
+  Medium.create(
+    title: row['Title'],
+    synopsys: row['Description'],
+    duration: row['Runtime (Minutes)'],
+    year: row['Year']
+    press_rating: row['Metascore'].to_f / 10
+    audience_rating: row['Rating']
+    )
+end
