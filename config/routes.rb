3dc62f3e3634ee3978eea3d49ae6f32792d70033
Arthur GAUTHIER <40882617+ArthurGAUTHIER@users.noreply.github.com>
2018-08-28 18:36:49 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :medium, only: [:index, :show]
+  get '/discover', to: 'media#index', as: 'discover'
+  
+  get '/dashboard', to: 'users#show', as: 'dashboard'
   devise_for :users
+  
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
