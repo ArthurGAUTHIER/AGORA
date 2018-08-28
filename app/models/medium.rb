f4c9f29b91b6e40f27e88efb23928816d1de397a
Julien Ducrocq <38463235+Nijelu@users.noreply.github.com>
2018-08-28 19:17:00 +0200

    Update medium.rb

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,9 @@
 class Medium < ApplicationRecord
   belongs_to :studio
+  has_many :media_actor
+  has_many :media_director
+  has_many :media_mood
+  has_many :media_category
   has_many :actors, through: :media_actor
   has_many :directors, through: :media_director
   has_many :moods, through: :media_mood
