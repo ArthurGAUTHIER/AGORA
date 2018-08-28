612815848557f99af3ce7158c3a9f195d8a049f6
Julien Ducrocq <38463235+Nijelu@users.noreply.github.com>
2018-08-28 19:09:58 +0200

    Update actor.rb

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ApplicationRecord
   validates :last_name, presence: true, uniqueness: { scope: :first_name }
   has_many :media_actors
+  has_many :media, through: :media_actors 
 end
