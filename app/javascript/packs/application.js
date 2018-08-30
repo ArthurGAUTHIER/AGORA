cba64240b1d5687e9c2d8243215261f43ed819f2
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-08-30 16:36:56 +0200

    que_de_la_merde

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+
+const buttons = document.querySelectorAll('.tablinks')
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    console.log(event.target.innerText)
+  })
+})
