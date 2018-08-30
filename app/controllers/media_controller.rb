85bfaf81c83f1ab2d63b12c11e317f8932ec6445
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-08-30 12:18:26 +0200

    prod banner changed + controller half for media#index

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,2 +1,34 @@
 class MediaController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    if advanced_search?
+      @media = Medium.all #TOCHANGE
+    elsif simple_search?
+      @media = Medium.search_in_all(params[:query])
+    else
+      @media = Medium.all
+    end
+  end
+
+  private
+
+  def advanced_search?
+    params[:category] ||
+    params[:actor] ||
+    params[:director] ||
+    params[:mood] ||
+    params[:studio] ||
+    params[:year] ||
+    params[:duration] ||
+    params[:recent] ||
+    params[:seen] ||
+    params[:netflix] ||
+    params[:amazon]
+
+  end
+
+  def simple_search?
+    params[:query]
+  end
 end
