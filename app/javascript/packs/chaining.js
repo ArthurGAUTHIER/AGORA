7c620c88b65426040c82598eceaef8812cac336c
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-09-01 17:22:19 +0200

    Front done

---
+++ app/javascript/packs/chaining.js
@@ -0,0 +1,7 @@
+document.getElementById('back').addEventListener('click', (event) => {
+  console.log('back clicked')
+});
+
+document.getElementById('next').addEventListener('click', (event) => {
+  console.log('next clicked')
+});
