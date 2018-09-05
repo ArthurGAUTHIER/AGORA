af8aed600b1f80f9c84dbf1b851364d5bb6690c7
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-09-05 11:17:56 +0200

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   def show
+
   end
 
   def user_params
     params.require(:article).permit(:alias, :email, :photo)
+  end
+
+  def media_count
+    Library.all.where(already_watched: true, user: current_user).each do |instance|
+      return instance.count
+    end
+  end
+
+  def time_spent
+  end
+
+  def favorite
+  end
+
+  def average_duration
   end
 end
