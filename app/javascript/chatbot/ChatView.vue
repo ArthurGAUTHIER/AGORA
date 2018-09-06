f8be570cbf1f39d04c122e75fe27a9bf0d3a7194
Yann IRBAH <yann@woodcut.io>
2018-09-06 23:43:02 +0200

    Implements a VueJS chatbot

--- app/javascript/chatbot/ChatView.vue
+++ app/javascript/chatbot/ChatView.vue
@@ -1,15 +1,19 @@
 <template>
-<div>
   <div class="chatview">
     <div class="messages">
-      <div v-for="message in messages" :key="message">{{ message }}</div>
+      <div v-for="message in messages" :key="message.id">
+        <div class="chatMessage" :class="message.side">
+          <div>
+            {{ message.content }}
+          </div>
+        </div>
+      </div>
     </div>
     <div class="input">
       <input type="text" v-model="message">
       <button @click="sendMessage">SEND</button>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -29,18 +33,28 @@ export default {
 
   methods: {
     sendMessage() {
+      this.messages.push({
+        side: 'sent',
+        content: this.message,
+        id: (this.messages.length + 1),
+      });
+
+      const newMessage = this.message;
+      this.message = '';
+
       axios.post('/bot/send', {
         conversation_id: this.conversationId,
         message: {
           type: 'text',
-          content: this.message
+          content: newMessage,
         }
-      } )
+      })
         .then(({data}) => {
-          this.message = '';
           data.response.messages.forEach((message) => {
-            console.log(message);
-            this.messages.push(message.content);
+            if (message.content.match(/\$chiengeant/)) {
+              window.location.href = '/discover';
+            }
+            this.messages.push({...message, side: 'received', id: (this.messages.length + 1) });
           })
         })
         .catch(function (error) {
@@ -53,21 +67,56 @@ export default {
 
 <style lang="scss" scoped>
 .messages {
-  height: 30vh;
-  width: 100%;
+  margin-top: 30px;
+  height: 50vh;
+  width: 700px;
   background: #FFFFFF;
   margin-bottom: 20px;
+  padding: 20px;
+  overflow: scroll;
 }
 
 .input {
   display: flex;
+}
 
-  input {
-    width: 80%;
+input {
+  width: 80%;
+  height: 50px;
+  padding: 2px 10px;
+}
+
+button {
+  width: 20%;
+  background: rgb(181, 7, 7);
+  color: #FFFFFF;
+  border: none;
+}
+
+.chatMessage {
+  display: flex;
+  margin-bottom: 10px;
+  div {
+    border-radius: 10px;
+    width: 70%;
+    padding: 20px 5px;
   }
 
-  button {
-    width: 20%;
+  &.received {
+    justify-content: flex-start;
+
+    div {
+      color: #FFFFFF;
+      background: rgb(181, 7, 7);
+    }
+  }
+
+  &.sent {
+    justify-content: flex-end;
+
+    div {
+      background: lightgray;
+    }
   }
 }
 
